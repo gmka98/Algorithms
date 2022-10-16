3fe9b34240eead5f1e77d570ebcb94b2d6f5c638
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2022-10-16 23:54:08 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,60 @@
+/*I- calculate
+Write an algorithm which calculates:*/
+
+/*the sum of two chosen number*/
+var one = 78
+var two = 56
+console.log(one+two);
+
+/*the division of two chosen number*/
+
+var div = 81
+var ision = 9
+console.log(div/ision)
+
+/*the modulo of two inputed numbers*/
+
+var mod = 75
+var ulo = 13
+console.log(mod%ulo)
+
+/*II - concatenate sentence
+Write an algorithm which concatenates two phrases together.*/
+var slt = "Bienvenue"
+var cv = "Dans mon espace personel"
+console.log(slt+cv)
+
+/*III - VAT
+Write an algorithm which receives a price without VAT and returns the price with VAT with a rate of 21%.*/
+var price = 100
+var vat = 21/100
+console.log(price+vat)
+
+/*IV - surface of a circle
+Write an algorithm which receives the radius of a circle and calculate its surface.*/
+
+
+/*V - conversion of time to seconds
+Write an algorithm which receives the time of day in three different numbers, the hour, the minutes 
+and the seconds and returns the number of seconds since midnight.*/
+
+/*I - cinema tariffs
+In a cinema the full tariff is 10 €, the reduced one is 8 €. 
+Write an algorithm which given a boolean indicating 
+if the person can have a reduced tariff prints the price to pay.*/
+
+/*II - maximum
+Write an algorithm which given 3 numbers finds the maximum.*/
+
+/*III - identical dice
+Write an algorithm which throws 3 dices and finds out 
+the number of identical value, three, two or none.*/
+
+/*IV - day’s number
+Write an algorithm which given the number of a day returns its name.*/
+
+/*V - print shop
+A print shop charges 0.12 € the ten first copy, 0.11 € the next 20 and 0.10 € from there.
+ Write an algorithm which given a number of copies and calculates the price.*/
+
+
